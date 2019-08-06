3d743b0cb6abfc638bc498cb7fa9ffc26d284d4c
SashaMargaryan <sasha.margaryan@inbox.r>
2019-08-06 23:17:27 +0400

    homvork

---
+++ 7.py
@@ -0,0 +1,5 @@
+import math
+x=float(input("trvache:x"))
+y=float(input("trvache:y"))
+
+print("luchum:",math.pow(x,1/6)+math.pow(y,1/6)+math.pow(4,1/12)+math.fabs(math.pow(x,10)+math.fabs(math.pow(y,10))))
